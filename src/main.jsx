724b8e6f6bb0d9dbe1b7af006da542886ff44c6c
OzNiT <hugorobert.pro@icloud.com>
2025-04-09 15:47:27 +0200

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import GoTradeWeekly from './App.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <GoTradeWeekly />
+  </React.StrictMode>
+);
